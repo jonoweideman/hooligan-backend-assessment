b1bf6b79731470d0acc532ed2dc8e7cf9027916c
ExplorerJono <jonathanw@explore-ai.net>
2022-08-02 16:38:42 +0200

    Add working increment for existing user_id

--- index.js
+++ index.js
@@ -31,29 +31,41 @@ app.post('/', (req, res) => {
     fetchSession(params)
         .then(item => {
             console.log(item);
-            if (item.Item && item.Item.active_sessions.N < 3) {
-                res.status(200).json({
-                    message: "Found user and can increase session count."
-                });
-            } else if (item.Item && item.Item.active_sessions.N == 3) {
-                res.status(200).json({
-                    message: "Max sessions reached."
-                });
+            const active_sessions = parseInt(item.Item.active_sessions.N)
+            if (item.Item && active_sessions < 3) {
+                increaseSession(user_id, active_sessions).then(() => {
+                    res.status(200).json({
+                        message: `Number of active session incremented to ${active_sessions + 1}`
+                    }).send();
+                }).catch(err => {
+                    res.status(500).json({
+                        message: err
+                    }).send();
+                })
+            } else if (item.Item && active_sessions == 3) {
+                res.status(409).json({
+                    message: "Max sessions of 3 reached."
+                }).send();
             } else {
                 //user does not exist
-                res.status(200).json({
-                    message: "User does not exist and must be created."
-                });
+                increaseSession(user_id, 0).then(() => {
+                    res.status(200).json({
+                        message: `Number of active session incremented to 1`
+                    }).send();
+                }).catch(err => {
+                    res.status(500).json({
+                        message: err
+                    }).send();
+                })
             }
         })
         .catch(err => {
             console.log(err);
-            res.status(500).json({
-                message: err
-            });
+            // res.status(500).json({
+            //     message: err
+            // });
         })
         .finally(obj => {
-            res.send();
         })
 });
 
@@ -65,7 +77,18 @@ const fetchSession = async (params) => {
     return ddb.getItem(params).promise();
 }
 
-
+const increaseSession = async (user_id, active_sessions) => {
+    console.log(`active session: ${active_sessions}`)
+    var params = {
+        TableName: TABLE_NAME,
+        Item: {
+            'user_id': { N: user_id },
+            'active_sessions': { N: (active_sessions + 1).toString() }
+        }
+    };
+    console.log(params)
+    return ddb.putItem(params).promise();
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`);
